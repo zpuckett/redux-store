cf566790de562f3861c0b789c3ab5296d48d271d
Zach Puckett <zpuckettdesign@gmail.com>
2021-07-18 13:40:35 -0500

    add items to cart

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
+import { useStoreContext } from '../../utils/GlobalState';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 function ProductItem(item) {
   const {
@@ -10,6 +12,14 @@ function ProductItem(item) {
     price,
     quantity
   } = item;
+  const [state, dispatch] = useStoreContext();
+
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: {...item, purchaseQuantity: 1}
+    });
+  };
 
   return (
     <div className="card px-1 py-1">
@@ -24,7 +34,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button>Add to cart</button>
+      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,7 +6,12 @@ import Cart from '../components/Cart';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 import {useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -17,6 +22,13 @@ function Detail() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
   
   const { products } = state;
+  
+  const addToCart = () => {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  };
   
   useEffect(() => {
     if (products.length) {
@@ -41,7 +53,7 @@ function Detail() {
 
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
+            <button onClick={addToCart}>Add to Cart</button>
             <button>Remove from Cart</button>
           </p>
 
