9f876759eb8f08436c2c898daa8a44d2b1ccdc3b
Zach Puckett <zpuckettdesign@gmail.com>
2021-07-18 11:33:20 -0500

    useState

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -11,13 +11,13 @@ const StoreProvider = ({ value = [], ...props }) => {
         categories: [],
         currentCategory: '',
     });
-    const useStoreContext = () => {
-        return useContext(StoreContext);
-    }
-
     //use this to confirm it works!
     console.log(state);
     return <Provider value = {[state, dispatch]} {...props} />;
 };
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+}
 
 export { StoreProvider, useStoreContext};
