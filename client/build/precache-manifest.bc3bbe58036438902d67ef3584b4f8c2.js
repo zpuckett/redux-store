12186c311e18b724fc415824ab84284431751591
Zach Puckett <zpuckettdesign@gmail.com>
2021-07-22 12:51:36 -0500

    build wip

---
+++ client/build/precache-manifest.bc3bbe58036438902d67ef3584b4f8c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4beef5a1ea2e1d07ff9936c0a590b80b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e5344c8cd9de022dbb6",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "11d33ee044085d97e603",
+    "url": "/static/js/2.7525606e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.7525606e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5344c8cd9de022dbb6",
+    "url": "/static/js/main.d15c0b00.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
